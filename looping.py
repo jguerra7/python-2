69d8b8fbfec783edf28288539283a390c3f3f8ad
jlafleur <joel.n.lafleur@gmail.com>
2017-05-03 07:40:56 -0500

    more looping

--- looping.py
+++ looping.py
@@ -9,3 +9,26 @@ for i in data_list:
 print("\nFactorials")
 n = 5
 print("Factorial of", n, "is", math.factorial(n))
+
+
+print("\nCount strings")
+
+
+def count_letters(the_string, upper_or_lower=""):
+    upper = 0
+    lower = 0
+    for l in the_string:
+        if l.isupper():
+            upper += 1
+        elif l.islower():
+            lower += 1
+
+    if upper_or_lower == "upper":
+        return upper
+    elif upper_or_lower == "lower":
+        return lower
+
+my_str = "\"This string has letters\""
+print(my_str, "is the string.")
+print("Upper letters:", count_letters(my_str, "upper"))
+print("Lower letters:", count_letters(my_str, "lower"))
