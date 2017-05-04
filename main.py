728e2f253f5842995ed473bbf0e99565193dbf64
jlafleur <joel.n.lafleur@gmail.com>
2017-05-04 15:08:26 -0500

    Got bicycle lab working, and then broke it again

---
+++ main.py
@@ -0,0 +1,14 @@
+from bicycle import Bicycle
+from bicycle import Customer
+from bicycle import BikeShop
+
+
+bike_shop.print_inventory()
+
+bike_shop.sell_bike(2, customer1)
+
+bike_shop.print_inventory()
+bike_shop.show_profits()
+
+customer1.print_inventory()
+customer1.print_budget()
